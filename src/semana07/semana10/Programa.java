5db6bdb6f8fa84b3d17f6e24f31241b9bd09bc83
Ana <oliveira.camardella@aluno.ifsp.edu.br>
2023-05-02 14:54:50 -0300

    semana 10

--- src/semana07/semana10/Programa.java
+++ src/semana07/semana10/Programa.java
@@ -17,6 +17,38 @@ class Retangulo {
 
 }
 
+class Quadrado {
+    //Atributos
+    double lado;
+
+
+    //Métodos
+    public double calcularArea() {
+        return lado*lado;
+    }
+
+    public double calcularPerimetro (){
+        return 4*lado;
+    }
+
+}
+
+class Circulo {
+    //Atributos
+    double raio;
+
+
+    //Métodos
+    public double calcularArea() {
+        return Math.PI*(raio*raio);
+    }
+
+    public double calcularPerimetro (){
+        return 2* Math.PI*raio;
+    }
+
+}
+
 public class Programa{
     public static void main(String[] args){
 
@@ -30,11 +62,23 @@ public class Programa{
         ret2.base = 30.0;
         ret2.altura = 100.0;
 
+        Quadrado quad1 = new Quadrado();
+        quad1.lado= 15.0;
+
+        Circulo circ1 = new Circulo();
+        circ1.raio=5.0;
+
         System.out.println(ret1.calcularArea());
         System.out.println(ret1.calcularPerimetro());
 
         System.out.println(ret2.calcularArea());
         System.out.println(ret2.calcularPerimetro());
+
+        System.out.println(quad1.calcularArea());
+        System.out.println(quad1.calcularPerimetro());
+
+        System.out.println(circ1.calcularArea());
+        System.out.println(circ1.calcularPerimetro());
 
 
 
