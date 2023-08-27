79eee4ed44d7672355a88c37c0a999c317555d96
Ana Luísa <125326119+tiredmoth@users.noreply.github.com>
2023-08-27 15:36:04 -0300

    Update Prova.java

--- src/semana20/exercicios/Prova.java
+++ src/semana20/exercicios/Prova.java
@@ -28,24 +28,5 @@ public class Prova {
     }
 }
 
-public class Main {
-
-    public static void main(String[] args) {
-        Prova prova = new Prova();
-
-        QuestaoVerdadeiroFalso questaoVF = new QuestaoVerdadeiroFalso(1, "Java é uma linguagem de programação.", true);
-        QuestaoUnicaEscolha questaoUE = new QuestaoUnicaEscolha(2, "Qual é a capital da França?", "Paris");
-        List<String> opcoesCorretas = new ArrayList<>();
-        opcoesCorretas.add("Laranja");
-        opcoesCorretas.add("Maçã");
-        QuestaoMultiplaEscolha questaoME = new QuestaoMultiplaEscolha(3, "Quais frutas são amarelas?", opcoesCorretas);
-
-        prova.adicionarQuestao(questaoVF);
-        prova.adicionarQuestao(questaoUE);
-        prova.adicionarQuestao(questaoME);
-
-        prova.realizarProva();
-    }
-}
 
 
